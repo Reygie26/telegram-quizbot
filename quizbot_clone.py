7d47154cf9f9186d4961f85eb286e48e64459691
Reygie M. Gorgonio <97387345+Reygie26@users.noreply.github.com>
2026-07-03 14:25:43 +0800

    Update quizbot_clone.py

--- quizbot_clone.py
+++ quizbot_clone.py
@@ -1028,16 +1028,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message:
         context.user_data["chat_messages"].append(update.message.message_id)
 
+    BTN_PADDING = "\u2800" * 10
     keyboard = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("🧠 TeleQuiz", callback_data="SELECT_BOT_TELEQUIZ"),
-            InlineKeyboardButton("🎬 VideoGen", callback_data="SELECT_BOT_VIDEOGEN"),
+            InlineKeyboardButton("🧠 TeleQuiz{BTN_PADDING}", callback_data="SELECT_BOT_TELEQUIZ"),
+            InlineKeyboardButton("🎬 VideoGen{BTN_PADDING}", callback_data="SELECT_BOT_VIDEOGEN"),
         ]
     ])
 
-    PADDING = "\u2800" * 10
     msg = await update.message.reply_text(
-        f"Please Choose a Bot to Use:{PADDING}",
+        f"Please Choose a Bot to Use:",
         reply_markup=keyboard
     )
 
